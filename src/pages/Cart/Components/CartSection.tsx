88a0e4899c2cb85af0e891254eb99c520990ef2e
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-06-16 04:45:13 -0300

    feat: add CartPage route

---
+++ src/pages/Cart/Components/CartSection.tsx
@@ -0,0 +1,9 @@
+const CartSection = () => {
+  return (
+    <section className="h-screen py-[4.875rem] px-[6.25rem]">
+
+    </section>
+  )
+}
+
+export default CartSection
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,14 +2,15 @@ import { Route, Routes } from "react-router-dom"
 import Homepage from "../pages/Homepage/Homepage"
 import SingleProductPage from "../pages/SingleProduct/SingleProductPage"
 import Shop from "../pages/Shop/Shop"
-
+import CartPage from "../pages/Cart/CartPage"
 
 export const MainRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Homepage/>}/>
-            <Route path="/shop" element={<Shop/>}/>
-            <Route path="/product/:productId" element={<SingleProductPage />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/product/:productId" element={<SingleProductPage />} />
+      <Route path="/cart" element={<CartPage />} />
+    </Routes>
+  )
 }
